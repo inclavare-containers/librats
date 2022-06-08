b918430269d12336dfd6b78eb55d56d396f6aa8b
Liang Yang <liang3.yang@intel.com>
2022-06-08 12:28:05 +0800

    librats: add nullverifier function
    Signed-off-by: Liang Yang <liang3.yang@intel.com>
    Signed-off-by: Shirong Hao <shirong@linux.alibaba.com>

---
+++ verifiers/nullverifier/main.c
@@ -0,0 +1,41 @@
+/* Copyright (c) 2022 Intel Corporation
+ * Copyright (c) 2020-2021 Alibaba Cloud
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdio.h>
+#include <librats/verifier.h>
+#include <librats/log.h>
+
+extern rats_verifier_err_t rats_verifier_register(rats_verifier_opts_t *);
+extern rats_verifier_err_t nullverifier_pre_init(void);
+extern rats_verifier_err_t nullverifier_init(rats_verifier_ctx_t *);
+extern rats_verifier_err_t nullverifier_verify_evidence(rats_verifier_ctx_t *,
+							attestation_evidence_t *, uint8_t *,
+							uint32_t hash_len);
+extern rats_verifier_err_t nullverifier_cleanup(rats_verifier_ctx_t *);
+
+static rats_verifier_opts_t nullverifier_opts = {
+	.api_version = RATS_VERIFIER_API_VERSION_DEFAULT,
+	.flags = RATS_VERIFIER_OPTS_FLAGS_DEFAULT,
+	.name = "nullverifier",
+	.priority = 0,
+	.pre_init = nullverifier_pre_init,
+	.init = nullverifier_init,
+	.verify_evidence = nullverifier_verify_evidence,
+	.cleanup = nullverifier_cleanup,
+};
+
+#ifdef SGX
+void libverifier_null_init(void)
+#else
+void __attribute__((constructor)) libverifier_null_init(void)
+#endif
+{
+	RATS_DEBUG("called\n");
+
+	rats_verifier_err_t err = rats_verifier_register(&nullverifier_opts);
+	if (err != RATS_VERIFIER_ERR_NONE)
+		RATS_ERR("failed to register the rats verifier 'nullverifier' %#x\n", err);
+}
