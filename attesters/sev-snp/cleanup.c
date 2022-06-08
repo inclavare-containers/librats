1fbaf5177370c777c12421620d9531fc35e0e09c
Liang Yang <liang3.yang@intel.com>
2022-06-08 12:24:08 +0800

    librats: add AMD sev and sev-snp attester function
    Signed-off-by: Shirong Hao <shirong@linux.alibaba.com>

---
+++ attesters/sev-snp/cleanup.c
@@ -0,0 +1,15 @@
+/* Copyright (c) 2022 Intel Corporation
+ * Copyright (c) 2020-2022 Alibaba Cloud
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <librats/attester.h>
+#include <librats/log.h>
+
+rats_attester_err_t sev_snp_attester_cleanup(rats_attester_ctx_t *ctx)
+{
+	RATS_DEBUG("called\n");
+
+	return RATS_ATTESTER_ERR_NONE;
+}
