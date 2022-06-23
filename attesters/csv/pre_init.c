051fe965eed5e32d5bbd0ee69260a9fec4d4d75f
Shirong Hao <shirong@linux.alibaba.com>
2022-06-23 14:28:26 +0800

    librats: add csv attester instance
    The csv attester instance runs in the CSV(2) guest and is
    responsible for collecting the evidence of CSV(2) guest.
    
    Fixes: #6
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Signed-off-by: Shirong Hao <shirong@linux.alibaba.com>

---
+++ attesters/csv/pre_init.c
@@ -0,0 +1,23 @@
+/* Copyright (c) 2022 Hygon Corporation
+ * Copyright (c) 2020-2022 Alibaba Cloud
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <librats/attester.h>
+#include <librats/log.h>
+
+rats_verifier_err_t csv_attester_pre_init(void)
+{
+	RTLS_DEBUG("called\n");
+
+	rats_verifier_err_t err = RATS_ATTESTER_ERR_NONE;
+
+	char *cmdline_str = "which wget 1> /dev/null 2> /dev/null";
+	if (system(cmdline_str) != 0) {
+		RTLS_ERR("please install wget for csv attest\n");
+		err = -RATS_ATTESTER_ERR_NO_TOOL;
+	}
+
+	return err;
+}
